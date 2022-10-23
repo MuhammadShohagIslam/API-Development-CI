66f0f66a68f065e13fe602aaa695cbaaef4fda24
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-10-23 15:27:50 +0300

    Done User API Swagger Implementation

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import express,{Request, Response} from "express";
+import express, { Request, Response } from "express";
 import { json, urlencoded } from "body-parser";
+import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 import "dotenv/config";
 import { successLogger } from "./config/logger";
@@ -9,10 +10,9 @@ import { processCorrelationId } from "./middlewares/process-correlation-id";
 
 const app = express();
 
-app.use([json(), urlencoded({ extended: false })]);
+app.use([json(), urlencoded({ extended: false }), cors()]);
 
 app.use(processCorrelationId);
-
 
 if (process.env.ENVIRONMENT != "TEST") {
     app.use(successLogger());
@@ -23,9 +23,7 @@ rootRouters(app);
 app.use(errorHandler);
 
 app.use("/api-docs", swaggerUi.serve, async (_req: Request, res: Response) => {
-    return res.send(
-        swaggerUi.generateHTML(await import("./swagger.json"))
-    );
+    return res.send(swaggerUi.generateHTML(await import("./swagger.json")));
 });
 
 export default app;

ed4fdde2c9260343bd81fdb8a30aaa71c1094399
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-11-01 22:30:24 +0300

    fix issue mongoose method

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,9 @@ import { json, urlencoded } from "body-parser";
 import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 import "dotenv/config";
-import { successLogger, errorLogger } from "./config/logger.config";
-import rootRouters from "./routers";
+// import { successLogger, errorLogger } from "./config/logger.config";
 import errorHandler from "./middlewares/error-handler";
+import rootRouters from "./routers";
 import { processCorrelationId } from "./middlewares/process-correlation-id";
 import "./services/cache";
 
@@ -16,15 +16,15 @@ app.use([json(), urlencoded({ extended: false }), cors()]);
 
 app.use(processCorrelationId);
 
-if (process.env.ENVIRONMENT != "TEST") {
-    app.use(successLogger());
-}
+// if (process.env.ENVIRONMENT != "TEST") {
+//     app.use(successLogger());
+// }
 
 rootRouters(app);
 
-if (process.env.ENVIRONMENT != "TEST") {
-    app.use(errorLogger());
-}
+// if (process.env.ENVIRONMENT != "TEST") {
+//     app.use(errorLogger());
+// }
 
 app.use(errorHandler);
 
